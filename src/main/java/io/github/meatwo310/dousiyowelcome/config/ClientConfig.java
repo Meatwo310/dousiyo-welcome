eedb5244e39889200121c100ce433c11d896019b
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-09-03 00:30:39 +0900

    -1

--- src/main/java/io/github/meatwo310/dousiyowelcome/config/ClientConfig.java
+++ src/main/java/io/github/meatwo310/dousiyowelcome/config/ClientConfig.java
@@ -6,8 +6,8 @@ public class ClientConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     public static final ForgeConfigSpec.IntValue AGREED_LAW_VERSION = BUILDER
-            .comment("The version of the law that the user has agreed to.")
-            .defineInRange("agreedLawVersion", 0, 0, 1);
+            .comment("The version of the mod that the user has agreed to.")
+            .defineInRange("agreedLawVersion", -1, -1, 0);
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 }
